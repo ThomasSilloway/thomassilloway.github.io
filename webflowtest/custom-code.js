a1c4cabcfa2102f775414c164c626877c38798fd
ThomasSilloway <tcsillow@gmail.com>
2022-12-23 18:39:25 -0500

    Data is sent, but still cors exception

--- webflowtest/custom-code.js
+++ webflowtest/custom-code.js
@@ -14,27 +14,25 @@ el
 var form = $(el);
 form.submit(function (e) {
     e.preventDefault();
-    form = $(e.target);
-    var data = convertFormToJSON(form);
-    var action = form.attr("action");
-    $.ajax({
-    url: action,
-    method: "POST",
-    data: JSON.stringify(data),
-    contentType: "application/json",
-    dataType: "json",
-    success: function () {
+
+    var request_url = "https://maker.ifttt.com/trigger/email_form/with/key/juU6Hiz3TA3u9DGHH0fjicOfZ9EpdVzPREW7VElIIcX";
+
+    var request_data = {
+        value1 : "test7",
+        value2 : "test8",
+        value3 : "test9"
+    };
+
+    $.post(request_url, request_data).done(function( data ) {
         var parent = $(form.parent());
         // Hide the form
         parent.children("form").css("display", "none");
         // Display the "Done" block
         parent.children(".w-form-done").css("display", "block");
-    },
-    error: function () {
+    }).fail(function() {
         var parent = $(form.parent());
         // Display the "Failed" block
         parent.find(".w-form-fail").css("display", "block");
-    },
     });
 });
 });
