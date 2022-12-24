a62f38756d73fa37956ddb7c83f8533b20714899
ThomasSilloway <tcsillow@gmail.com>
2022-12-23 20:07:48 -0500

    Add ability to send without using email quota

--- webflowtest/custom-code.js
+++ webflowtest/custom-code.js
@@ -1,3 +1,19 @@
+var deployment = "test";
+//var deployment = "prod";
+
+function is_test()
+{
+    return deployment == "test";
+}
+
+function displaySuccess(parent)
+{
+    // Hide the form
+    parent.children("form").css("display", "none");
+    // Display the "Done" block
+    parent.children(".w-form-done").css("display", "block");
+}
+
 $('#email-form').each(function (
 i,
 el
@@ -5,18 +21,21 @@ el
 var form = $(el);
 form.submit(function (e) {
     e.preventDefault();
+
     form = $(e.target);
+
+    if(is_test())
+    {
+        displaySuccess(form.parent());
+        return;
+    }    
 
     var request_url = "https://hook.us1.make.com/r9cdtqyxifkvsd9fzeybbau5mijyy1wb";
 
-    var request_data = form.serializeArray();;
+    var request_data = form.serializeArray();
 
     $.post(request_url, request_data).done(function( data ) {
-        var parent = $(form.parent());
-        // Hide the form
-        parent.children("form").css("display", "none");
-        // Display the "Done" block
-        parent.children(".w-form-done").css("display", "block");
+        displaySuccess($(form.parent()));
     }).fail(function() {
         var parent = $(form.parent());
         // Display the "Failed" block
